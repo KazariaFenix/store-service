9153762058c8002d252535f4fc373672e8bbd54c
Felix0417 <felix.the.cat.msk@gmail.com>
2023-12-19 00:26:11 +0300

    Add entity Add repository, service and controller layers Add some dependencies Add docker-compose to bring up a postgres

--- src/main/java/aston/red/storeservice/StoreServiceApplication.java
+++ src/main/java/aston/red/storeservice/StoreServiceApplication.java
@@ -2,10 +2,9 @@ package aston.red.storeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class StoreServiceApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/aston/red/storeservice/dto/StoreDto.java
@@ -0,0 +1,24 @@
+package aston.red.storeservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class StoreDto {
+
+    private long id;
+
+    private String name;
+
+    private String description;
+
+    private String address;
+
+    private String phoneNumber;
+}
+
