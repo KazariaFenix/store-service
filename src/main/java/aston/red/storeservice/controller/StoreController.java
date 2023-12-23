096cbd5be68968a723504c3d85d9c01a7e7f3708
Felix0417 <felix.the.cat.msk@gmail.com>
2023-12-23 17:38:38 +0300

    Light refactor

--- src/main/java/aston/red/storeservice/controller/StoreController.java
+++ src/main/java/aston/red/storeservice/controller/StoreController.java
@@ -6,6 +6,7 @@ import aston.red.storeservice.service.StoreService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,8 @@ public class StoreController {
         return StoreMapper.INSTANCE.fromAllStoreToAllDto(service.getAll());
     }
 
-
+    @GetMapping(path = "/{id}")
+    public StoreDto getById(@PathVariable("id") long storeId) {
+        return StoreMapper.INSTANCE.fromStoreToDto(service.getById(storeId));
+    }
 }
